2b218356129877676619e6064f75e69bb4249349
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-05-01 14:37:13 +0300

    feat: queue, rfl

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,7 @@
+// @ts-nocheck
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +23,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value == k) {
+    if (l.next) {
+      return removeKFromList(l.next, k);
+    } else {
+      return null;
+    }
+  }
+  else {
+    const newList = new ListNode(l.value);
+    if (l.next) {
+      newList.next = removeKFromList(l.next, k);
+    }
+    return newList;
+  }
 }
 
 module.exports = {
